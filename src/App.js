787872dd6c58f9a26d97fd13b26ad5c3c6fa7aa8
Dhruvi Bakeri <bakeridhruvi@gmail.com>
2021-01-04 19:03:21 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import Routing from "./Components/Routing";
 
+import LogRocket from "logrocket";
+LogRocket.init("7uic5b/fishgame");
+
 function App() {
   return (
     <div className="App">
